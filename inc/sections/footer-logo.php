420b06dbd4d651c5c50fd56814e0ed6b156cba5e
ayan-matrix <ayanghosh@matrixnmedia.com>
2016-08-31 18:52:02 +0530

    work 31-8-2016

--- inc/sections/footer-logo.php
+++ inc/sections/footer-logo.php
@@ -71,7 +71,7 @@
 			) );
 			   
 		if ( class_exists( 'Tesseract_Remove_Branding' ) ) {
-		$unbrandinglogo = '<a href="http://tesseracttheme.com/unbranding-plugin-3/" target="_blank"><img src="https://s3.amazonaws.com/tesseracttheme/unlock.png" /></a>';
+		$unbrandinglogo = '<a href="#" target="_blank"><img src="ttp://tylers.s3.amazonaws.com/uploads/2016/08/10071350/Tyler-designstyle-smoothie-m.png" /></a>';
 
 		$wp_customize->add_setting( 'tesseract_footer_content_if_unbranding', array(
 			'sanitize_callback' => 'tesseract_sanitize_textarea_html',
