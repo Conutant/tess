9f1aa9c8fd2655d34d91d934684e252d8e92b7bb
ayan-matrix <ayanghosh@matrixnmedia.com>
2016-09-07 18:42:43 +0530

    work 7-9-2016

--- js/customize-controls.js
+++ js/customize-controls.js
@@ -1,15 +1,3 @@
 jQuery(document).ready(function($) {											
 
 	$("#accordion-panel-tesseract_social > ul > li > h3:contains('Social Account')").css('color', '#999');
-	
-	  
-	//$("[data-customize-setting-link=tesseract_header_layout_setting]").css("background-color", "yellow");
-	//Header Layout//////
- 	$('select[data-customize-setting-link="tesseract_header_layout_setting"]').change(function(){
-		 if(this.value=='centered-inline-logo')
-		 {//alert(this.value);
-		 }
-	     else{}
-		 }
-         );
-} )
