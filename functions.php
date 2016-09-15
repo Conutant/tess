6402ef47858f90cb9c507025c8aa5e5aac9d6c29
ayan-matrix <ayanghosh@matrixnmedia.com>
2016-09-15 18:54:51 +0530

    work 15-9-2016

--- functions.php
+++ functions.php
@@ -1087,8 +1087,10 @@ require get_template_directory() . '/inc/beaver-builder-modules/beaver-builder-m
 //Initialize the update checker.
 require 'theme-update-checker.php';
 $update_checker = new ThemeUpdateChecker(
-  'TESSERACT', // This theme folder name (must match)
-  'https://s3.amazonaws.com/tesseracttheme/version.json'
+  //'TESSERACT', // This theme folder name (must match)
+  //'https://s3.amazonaws.com/tesseracttheme/version.json'
+  'tesseract',
+  'http://tesseractplus.com/ryantest16/info.json'
 );
 if(false)
 {
@@ -1350,11 +1352,9 @@ deactivate_plugins( 'beaver-builder-lite-version/fl-builder.php' );
 } else {
 activate_plugins( 'beaver-builder-lite-version/fl-builder.php' );
 }
-
-function filter_plugin_updates( $value ) {
-    unset( $value->response['tesseractplus-plugin/fl-builder.php'] );
-	unset( $value->response['beaver-builder-lite-version/fl-builder.php'] );
-	unset( $value->response['siteorigin-panels/siteorigin-panels.php'] );
-    return $value;
-}
-add_filter( 'site_transient_update_plugins', 'filter_plugin_updates' );
+//Initialize the update checker.
+/*require 'theme-update-checker.php';
+$example_update_checker = new ThemeUpdateChecker(
+    'tesseract',
+    'http://testyourprojects.net/matrix/wpnew/aurora-safaritest/info.json'
+);*/
