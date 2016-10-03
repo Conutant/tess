6c70aeef11194b8b6dada08d0ac15a3cee9679a4
Amit <ayan@matrixnmedia.com>
2016-10-03 16:53:21 +0530

    work 03-10-2016

--- functions.php
+++ functions.php
@@ -1345,11 +1345,13 @@ function example_nag_ignore() {
 
 /* Deactive one plugin when other active */
 
-if ( is_plugin_active( 'tesseractplus-plugin/fl-builder.php' ) ) {
+/*if ( is_plugin_active( 'tesseractplus-plugin/fl-builder.php' ) ) {
 deactivate_plugins( 'beaver-builder-lite-version/fl-builder.php' );
 } else {
 activate_plugins( 'beaver-builder-lite-version/fl-builder.php' );
-}
+}*/
+
+
 //Initialize the update checker.
 /*require 'theme-update-checker.php';
 $example_update_checker = new ThemeUpdateChecker(
